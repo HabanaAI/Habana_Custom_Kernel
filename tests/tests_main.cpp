34ced7c4507622ce6bbf4f3e312bb9dcdff24c97
Zhongkai Zhang <74077786+zzhang37@users.noreply.github.com>
2023-04-05 11:07:00 -0700

    Added gather kernels for index addressing (#18)

--- tests/tests_main.cpp
+++ tests/tests_main.cpp
@@ -30,6 +30,7 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVE
 #include "avg_pool_2d_f32_test.hpp"
 #include "avg_pool_2d_f32_gaudi2_test.hpp"
 #include "cast_f16_to_i16_gaudi2_test.hpp"
+#include "gather_fwd_i32_test.hpp"
 
 int main(int argc, char** argv)
 {
@@ -68,6 +69,7 @@ int main(int argc, char** argv)
             "AddF32Test                 Run AddF32Test only   " << std::endl <<
             "AvgPool2DFwdF32Test        Run AvgPool2DFwdF32Test only   " << std::endl <<
             "AvgPool2DBwdF32Test        Run AvgPool2DBwdF32Test only   " << std::endl <<
+            "GatherFwdDim0I32Test       Run GatherFwdDim0I32Test only   " << std::endl <<
 
             "AvgPool2DFwdF32Gaudi2Test  Run AvgPool2DFwdF32Gaudi2Test only   " << std::endl <<
             "AvgPool2DBwdF32Gaudi2Test  Run AvgPool2DBwdF32Gaudi2Test only   " << std::endl <<
@@ -419,6 +421,23 @@ int main(int argc, char** argv)
         avgpool2df32ins.SetUp();
         result = avgpool2df32ins.runTest(GAUDI_KERNEL_AVG_POOL_2D_BWD_F32);
         avgpool2df32ins.TearDown();
+        testCount ++;
+        if (result != 0)
+        {
+            return result;
+        }
+    }
+
+    if(argc == 1 ||
+        (argc == 3 && (((strcmp(argv[1], "--device") ==0) || (strcmp(argv[1], "-d") ==0))
+        && (strcmp(argv[2],"Gaudi") ==0)))  ||
+        (argc == 3 && (((strcmp(argv[1], "--test") ==0) || (strcmp(argv[1], "-t") ==0))
+        && (strcmp(argv[2],"GatherFwdDim0I32Test") ==0))))
+    {
+        GatherFwdI32Test gatheri32ins;
+        gatheri32ins.SetUp();
+        result = gatheri32ins.runTest(GAUDI_KERNEL_GATHER_FWD_DIM0_I32);
+        gatheri32ins.TearDown();
         testCount ++;
         if (result != 0)
         {
